da251e943b5d2b3ae55e250f45813c7dc871bcb4
Luiz5007 <luizfelipe5007@gmail.com>
2020-06-02 10:33:00 -0300

    adjust

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+const [textareaJson] = document.getElementsByName("json");
+const [textareaCsv] = document.getElementsByName("csv");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+    const jsonObject =  textareaJson.value
+    
+    
+    
+})
+
+
+const bobo = [
+    {}
+]
