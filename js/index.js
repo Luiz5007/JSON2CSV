62a87417ce3dbd1bb6c6d5fe74b4379b9c0d09d5
Luiz5007 <luizfelipe5007@gmail.com>
2020-07-29 16:33:10 -0300

    json to csv ok

--- js/index.js
+++ js/index.js
@@ -2,16 +2,48 @@ const form = document.querySelector("form");
 const [textareaJson] = document.getElementsByName("json");
 const [textareaCsv] = document.getElementsByName("csv");
 
+
+const validateJsonObj = (jsonString) => {
+    try {
+        console.log(JSON.parse(jsonString));
+    } catch (error) {
+        throw new Error(error)        
+    }
+}
+
+const convertJsonToCsv = (jsonObjArray) => {
+    try {
+        let csv = ''
+
+        csv = csv + `${Object.keys(jsonObjArray[0])}\n`
+
+        for (const jsonObj of jsonObjArray) {
+            csv = csv + `${Object.values(jsonObj)}\n`
+        }
+
+        return csv
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    
-    const jsonObject =  textareaJson.value
-    
-    
-    
+    e.stopPropagation();
+
+    try {
+        const jsonString = textareaJson.value
+
+        validateJsonObj(jsonString)
+
+        const jsonObjArray = JSON.parse(jsonString)
+
+        
+        textareaCsv.value = `${convertJsonToCsv(jsonObjArray)}`       
+        
+        textareaJson.style.border = 'solid 1px black'  
+    } catch (error) {
+        textareaJson.style.border = 'solid 2px red'
+        textareaCsv.value = ''
+    }
 })
-
-
-const bobo = [
-    {}
-]
